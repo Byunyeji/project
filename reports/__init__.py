a90ccee1d94039c8c7c5114a8e83865ccefd1d37
yeji <yea03210@naver.com>
2025-04-20 16:05:28 +0900

    예지

--- reports/__init__.py
+++ reports/__init__.py
@@ -1,10 +1,17 @@
 # reports/__init__.py
 
-from .emotion_trend_plot    import plot_emotion_trend
-from .generate_report       import get_emotion_report, create_pdf_report, create_emotion_heatmap_data, calc_emotion_change
+from .emotion_trend_plot import plot_emotion_trend
+from .generate_report import (
+    get_emotion_report,
+    create_pdf_report,
+    create_emotion_heatmap_data,
+    calc_emotion_change
+)
 
 __all__ = [
     "plot_emotion_trend",
     "get_emotion_report",
     "create_pdf_report",
+    "create_emotion_heatmap_data",
+    "calc_emotion_change",
 ]
